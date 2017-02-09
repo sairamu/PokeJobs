550ab1326d622e9d27da1b915254b4f6e0442021
sairamu <sairam.u@hotmail.com>
2017-02-09 11:07:34 +0530

    phone and email working

--- phone_email.py
+++ phone_email.py
@@ -2,7 +2,7 @@ import nltk
 import os
 import re
 f = open('aks.txt').read()
-re_phone = re.findall(r'((?:\(?\+34\)?)?\d{12})',f) or (r'((?:\(?\+34\)?)?\d{10})',f)
+re_phone = re.findall(("\d{3}\d{3}\d{4}"),f) 
 print ('phone',re_phone)
 
 re_email = re.findall(r'[\w\-][\w\-\.]+@[\w\-][\w\-\.]+[a-z A-Z]{1,4}',f)
