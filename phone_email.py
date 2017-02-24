1ca08643809954cd92fb29c85499cc0cccb2f00b
sairamu <sairam.u@hotmail.com>
2017-02-24 14:23:34 +0530

    added resume folder

--- phone_email.py
+++ phone_email.py
@@ -1,10 +1,37 @@
 import nltk
 import os
 import re
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
 f = open('aks.txt').read()
-re_phone = re.findall(('(\(?\d{3}\D{0,3}\d{3}\D{0,3}\d{4}).*?'),f) 
-print ('phone',re_phone)
+re_phone = re.findall(('(\(?\d{3}\D{0,3}\d{3}\D{0,3}\d{4}).*?'),f)
+phone = str(re_phone)[1:-1] #converts list object to str
 
 re_email = re.findall(r'[\w\-][\w\-\.]+@[\w\-][\w\-\.]+[a-z A-Z]{1,4}',f)
-print ('email',re_email)
+#email = str(re_email).strip('[]') #converts list object to str
+email = re_email[0]
 
+msg = MIMEMultipart()
+msg['From'] = "pythonmailtesting@gmail.com"
+msg['To'] = email
+msg['Subject'] = "Welcome to PokeJobs"
+
+body = '''Hi ,
+
+Welcome to PokeJobs
+ '''
+
+msg.attach(MIMEText(body, 'plain'))
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
+server.starttls()
+#Next, log in to the server
+server.login("pythonmailtesting@gmail.com", "asdf123456789")
+text = msg.as_string()
+#Send the mail
+server.sendmail("pythonmailtesting@gmail.com", email, text)
+server.close()
