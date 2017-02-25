bcdd692bbb06214614122fff0a1e804af32c12e1
MurugesMarvel <murugeshmarvel95@zoho.com>
2017-02-25 19:44:46 +0530

    Started working on Input file stream

--- input_text.py
+++ input_text.py
@@ -1,8 +1,23 @@
-import numpy as np
-import nltk
-import abc
+#!/usr/bin/python
+'''This file parse all the resumes in the file named resume and convert into the feature vector and return them'''
 
 
-class chunk:
-  def __init__(self, filename):
-    self.filename = filename
+import os
+import numpy
+import glob
+
+class text_parse:
+	def read_file(self, filename):
+		self.file_name = filename
+		#direc = '/' + self.file_name
+		os.chdir(self.file_name)
+		for file in glob.glob('.doc'):
+			print file
+
+
+
+
+
+if __name__=='__main__':
+	text_parse_obj = text_parse()
+	text_parse_obj.read_file(filename='Resumes')
