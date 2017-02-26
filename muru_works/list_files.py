2a1fa3737f73997c95d23a1b79e631d580f8f2f1
MurugesMarvel <murugeshmarvel95@zoho.com>
2017-02-26 23:02:08 +0530

    fin * to pdf

--- muru_works/list_files.py
+++ muru_works/list_files.py
@@ -1,6 +1,9 @@
 
+
 import os
-i =0 
+import topdf
+i=1
+obj = topdf.pdf()
 def check_dot(name):
 	
 	if '.' in name: 
@@ -17,9 +20,11 @@ def sep_dot(file_name):
 	ext_name = file_name[(1+int(dot_index)):length]
 	return act_name, ext_name
 
-
-
-
+'''def doc2pdf(ori_name,new_name):
+	p = subprocess.Popen(['unoconv', '--stdout', ori_name], stdout=subprocess.PIPE)
+	with open(new_name, 'w') as output:
+	   shutil.copyfileobj(p.stdout, output)
+'''
 
 
 for filename in os.listdir('Resumes'):
@@ -28,10 +33,20 @@ for filename in os.listdir('Resumes'):
 	if tag == 'yes':
 		actual_name2, extension_name = sep_dot(extension_name)
 		actual_name = actual_name + actual_name2
+		original_name  = actual_name+'_'+actual_name2
 	else:
-		pass
-	print actual_name,"--",extension_name
+		original_name = actual_name
 
+	
+	original_name_pdf = original_name + '.pdf'
+	ori_filename = 'Resumes'+'/'+filename
+	dest_filename = 'resumes_pdf/'+original_name_pdf
+	if extension_name == 'doc':
+		print i
+		i+=1
+		#obj.doc2pdf(ori_filename,dest_filename)	
+		#print "converted ", original_name
+	
 
 
 '''    if filename.endswith(".doc") or filename.endswith(".py"): 
